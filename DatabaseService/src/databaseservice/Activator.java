a31afd05120c4a3c180c3b0b6b71488614a035f3
dilshanhiruna <dilshan.hirunaa@gmail.com>
2022-03-15 21:27:23 +0530

    Database Publisher

---
+++ DatabaseService/src/databaseservice/Activator.java
@@ -0,0 +1,22 @@
+package databaseservice;
+
+import org.osgi.framework.BundleActivator;
+import org.osgi.framework.BundleContext;
+import org.osgi.framework.ServiceRegistration;
+
+public class Activator implements BundleActivator {
+
+	private ServiceRegistration serviceRegistration;
+	
+	public void start(BundleContext context) throws Exception {
+		System.out.println("Database Publisher service started");
+		Database database = new DatabaseImpl();
+		serviceRegistration = context.registerService(Database.class.getName(), database, null);
+	}
+
+	public void stop(BundleContext context) throws Exception {
+		System.out.println("Database Publisher service stopped");
+		serviceRegistration.unregister();
+	}
+
+}
